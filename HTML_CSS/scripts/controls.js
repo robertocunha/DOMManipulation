d2b72d0acbce6bfc1640b868dad44b7f18451e74
Roberto Cunha <robertocunha@gmail.com>
2024-09-06 00:39:59 -0300

    Add radio buttons

--- HTML_CSS/scripts/controls.js
+++ HTML_CSS/scripts/controls.js
@@ -1,6 +1,8 @@
 const hideButton = document.getElementById("hideButtonId");
 const showButton = document.getElementById("showButtonId");
 const visibleCheckbox = document.getElementById("visibleCheckBoxId");
+const hideRadio = document.getElementById("hideRadioId");
+const showRadio = document.getElementById("showRadioId");
 
 const textP = document.getElementById("textPId");
 
@@ -9,7 +11,14 @@ let visible = true;
 function updateVisibility(visibleState) {
     visible = visibleState;
     textP.style.opacity = visible ? '1' : '0';
+
     visibleCheckbox.checked = visible;
+
+    if (visible) {
+        showRadio.checked = true;
+    } else {
+        hideRadio.checked = true;
+    }
 }
 
 hideButton.addEventListener("click", () => updateVisibility(false));
@@ -18,3 +27,6 @@ showButton.addEventListener("click", () => updateVisibility(true));
 visibleCheckbox.addEventListener("change", (event) => {
     updateVisibility(event.currentTarget.checked);
 });
+
+hideRadio.addEventListener("change", () => updateVisibility(false));
+showRadio.addEventListener("change", () => updateVisibility(true));
