0c323451dc5e3d2b7c27510e75b244d33cb22480
Roberto Cunha <robertocunha@gmail.com>
2024-09-05 22:49:50 -0300

    Remove console.log and empty lines

--- HTML_CSS/scripts/controls.js
+++ HTML_CSS/scripts/controls.js
@@ -1,8 +1,6 @@
 const hideButton = document.getElementById("hideButtonId");
 const showButton = document.getElementById("showButtonId");
-
 const textP = document.getElementById("textPId");
-console.log(textP);
 
 hideButton.addEventListener("click", () => {
     textP.style.display = "none";
